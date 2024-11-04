c9abdc6874ba4df0dec295eb1695bd7723743436
unknown <jes2kim6@gmail.com>
2024-11-04 16:55:37 +0800

    Attendance_v2.0

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -6,6 +6,11 @@
     <title>  Dashboards  | Enopoly Commerce</title>
 <!-- App favicon -->
 <link rel="shortcut icon" href="https://app.enopolyautomation.com/assets/images/favicon.ico">
+<link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+<!-- Responsive datatable examples -->
+<link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
   <!-- Bootstrap Css -->
 <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
@@ -15,7 +20,15 @@
 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+<style>
+  .dataTables_length {
+          margin-bottom: -1.5%;
+        }
+  .dataTables_info {
+   
+    margin-bottom: -1.8%;
+}     
+</style>
 <script>
         // Immediately apply dark mode if preferred
         if (localStorage.getItem('theme') === 'dark') {
@@ -53,8 +66,49 @@
 
 <script src="assets/js/pages/dashboard.init.js"></script>
 
+<!-- Required datatable js -->
+<script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="assets/libs/jszip/jszip.min.js"></script>
+<script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+        
+<!-- Responsive examples -->
+<script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+<!-- Datatable init js -->
+<script src="assets/js/pages/datatables.init.js"></script> 
+
+<script>
+$(document).ready(function() {
+    $('#akontable').DataTable({
+      pageLength: 10,
+        dom: 'lfBrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print' 
+        ],
+        paging: true,
+        searching: true,
+        ordering: true,
+        lengthMenu: [ 
+                    [5, 10, 15, 25, 50, 100],
+                    [5, 10, 15, 25, 50, 100]
+                ]
+    });
+});
+</script>
+
 
 <script src="assets/js/app.js"></script>
+
+
 
  
 
